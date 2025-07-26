6456c966c452f25df9aff93f19def14d501f2a2a
Arianit Likaj <arianitlikaj46@gmail.com>
2025-07-26 12:37:46 +0200

    first commit

---
+++ src/test/java/dev/rtko_ai/hr_service/HrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rtko_ai.hr_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
