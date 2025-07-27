e6bf4e3bb18728f7abe72d539f408d4b025b7465
agnesarama1 <agnesa.rama1@student.uni-pr.edu>
2025-07-27 10:45:02 +0200

    Created models and dto's for LeaveTypes

---
+++ src/main/java/dev/rtko_ai/hr_service/model/Leave.java
@@ -0,0 +1,29 @@
+package dev.rtko_ai.hr_service.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+@Entity
+@Table(name = "leave_requests")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Leave {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long employeeId;
+
+    @Enumerated(EnumType.STRING)
+    private LeaveType type;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private LeaveStatus status;
+}
